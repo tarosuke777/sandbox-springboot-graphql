9794d0606338938f93ae0e189550cfd9ff29d35b
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-08-12 16:22:27 +0900

    add list pattern

---
+++ src/main/java/com/example/graphqlserver/Friend.java
@@ -0,0 +1,5 @@
+package com.example.graphqlserver;
+
+public record Friend(String name) {
+
+}
--- src/main/java/com/example/graphqlserver/HeroController.java
+++ src/main/java/com/example/graphqlserver/HeroController.java
@@ -1,6 +1,9 @@
 package com.example.graphqlserver;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,5 +11,15 @@ public class HeroController {
     @QueryMapping
     public Hero hero() {
         return new Hero("Superman");
+    }
+
+    // @SchemaMapping
+    // public Friend[] friends(Hero hero) {
+    // return new Friend[] {new Friend("Batman"), new Friend("aquaman")};
+    // }
+
+    @SchemaMapping
+    public List<Friend> friends(Hero hero) {
+        return Arrays.asList(new Friend("Batman"), new Friend("aquaman"));
     }
 }
