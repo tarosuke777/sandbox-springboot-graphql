1fe24ab318a8a912cd0bb92c6937d90316ee4ea8
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-08-12 14:51:40 +0900

    add heroContoller

---
+++ src/main/java/com/example/graphqlserver/Hero.java
@@ -0,0 +1,5 @@
+package com.example.graphqlserver;
+
+public record Hero(String name) {
+}
+
---
+++ src/main/java/com/example/graphqlserver/HeroController.java
@@ -0,0 +1,12 @@
+package com.example.graphqlserver;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HeroController {
+    @QueryMapping
+    public Hero hero() {
+        return new Hero("Superman");
+    }
+}
